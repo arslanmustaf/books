f9899135e967a48f0c3a710557586c6ab755bb23
Muhammad Arslan <114974765+arslanmustaf@users.noreply.github.com>
2023-07-11 10:21:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ document.addEventListener("contextmenu", function (e) {
 }, false);
 
 
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  ${"#name"}.text('Name: ' + profile.getName())
+  ${"#email"}.text('Email :  ' + profile.getEmail())
+  ${"#image"}.attr('src', profile.getImageUrl())
+}
+
+
+
+
+
+
 let hamburger = document.querySelector(".hamburger")
 hamburger.addEventListener("click", ()=> {
   let navbar = document.querySelector(".header-bottom")
